5d638b06b3117306ec7d12b4f29e0f9771868197
Victor Hugo Rodrigues da Silva <victorq32@gmail.com>
2017-02-14 18:03:37 -0200

    exercício

--- Exercicio1/js/js.js
+++ Exercicio1/js/js.js
@@ -1,18 +1,23 @@
-//nome, idade e salario
-var nome = "Victor Hugo";
-var idade = 25;
-var salario = 2000;
-var cargo = "Tecnico de informatica";
+function calculaSalario(){
+		var nome = document.getElementById("nome").value,
+		idade = parseInt(document.getElementById("idade").value),
+		cargo = document.getElementById("cargo").value,
+		salario = parseFloat(document.getElementById("salario").value);
 
-	//o salario bruto teve reajuste de 38%
-var reajuste = salario * (38/100);
-	//o funcionário recebera uma gratificação de 20%
-var gratificacao = salario * (20/100);
-	//O salario total e descontado 15%
-var salarioTotal = salario + reajuste + gratificacao;
+		var reajuste = 0.38 * salario;
+		var gratificação = 0.20 * salario;
+		var salarioBruto = salario + reajuste + gratificação;
+		var salarioLiquido = salarioBruto - (0.15 * salarioBruto);
 
-var salarioLiquido = salarioTotal - (salarioTotal*(15/100));
 
-console.log(nome  + idade +  cargo );
-console.log(salario);
-console.log(salarioLiquido);
+		var solução = "<hr><p> nome: " + nome + "</p>" +
+									"<p> idade: " + idade + "</p>" +
+									"<p> cargo: " + cargo + "</p>" +
+									"<p> salario: " + salario + "</p>" +
+									"<p> salarioBruto: " + salarioBruto + "</p>" +
+									"<p> salarioLiquido: " + salarioLiquido + "</p>";
+
+
+		document.getElementById("resultado").innerHTML = solução;
+
+}
